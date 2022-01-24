043ce2d1f0906266b5dab10320a43e6dd0465143
Alvin1284 <81688085+Alvin1284@users.noreply.github.com>
2022-01-24 10:35:13 +0300

    initial commit

---
+++ Customer.py
@@ -0,0 +1,10 @@
+def customer_operations():
+    print("Welcome to Customer Operations")
+    operations = int(input("Select operations:\n "
+                           "[1] Load customer data to array\n"
+                           "[2] Insert customer\n"
+                           "[3] Delete customer\n"
+                           "[4] Update customer\n"
+                           "[5] Write Customer data to file\n"
+                           "[6] Exit\n"))
+
