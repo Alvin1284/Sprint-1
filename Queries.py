d596508c5e1fd30f3814a0f788fdc7358566f6ff
Alvin1284 <81688085+Alvin1284@users.noreply.github.com>
2022-02-01 22:06:21 +0300

    Queries functions

--- Queries.py
+++ Queries.py
@@ -10,16 +10,66 @@ def queries():
                                "[5]Quit"
                                ))
         if queries_op == 1:
-            pass
+            search_product()
         elif queries_op == 2:
-            pass
+            all_customers()
         elif queries_op == 3:
-            pass
+            all_products()
         elif queries_op == 4:
-            pass
+            list_customer()
         elif queries_op == 5:
             loop = 0
             pass
         else:
             print("Wrong input, Try again")
             queries()
+
+
+def search_product():
+    file = open("ProductData.txt", "r")
+    product_id = int(input("Enter product id: "))
+
+    s = ' '
+
+    while s:
+        s = file.readline()
+        li = s.split("~")
+        if len(s) > 0:
+            if int(li[0]) == product_id:
+                print("Product id:", li[0])
+                print("Product Name:", li[1])
+                print("Product Amount:", li[2])
+                print("Product Price:", li[3])
+
+
+def all_customers():
+    file = open("CustomerData.txt", "r")
+    s = ' '
+    s = file.readlines()
+
+    print(s)
+    pass
+
+
+def all_products():
+    file = open("ProductData.txt", "r")
+    s = ' '
+    s = file.readlines()
+
+    print(s)
+
+
+def list_customer():
+    file = open("CustomerData.txt", "r")
+    customer_id = int(input("Enter Customer id: "))
+
+    s = ' '
+
+    while s:
+        s = file.readline()
+        li = s.split("~")
+        if len(s) > 0:
+            if int(li[0]) == customer_id:
+                print("Customer id:", li[0])
+                print("Customer Name:", li[1])
+                print("Customer Address:", li[2])
